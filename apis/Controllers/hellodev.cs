a44a27ac0cfee92615f72ea6d9b38e55b5ff22d9
MHB80 <80700259+MHB80@users.noreply.github.com>
2023-11-23 13:26:55 +0330

    Add files via upload

---
+++ apis/Controllers/hellodev.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace apis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class hellodev : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("hello world");
+
+        [HttpPost]
+        public IActionResult Post(JSObject payload)
+        { 
+            return Ok (payload);
+        }
+    }
+
+}
